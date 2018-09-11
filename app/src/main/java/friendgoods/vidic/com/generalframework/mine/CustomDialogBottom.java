2edc0e63a7f3a75bd59b407f049fd12f1760d1bc
levy <2501908226@qq.com>
2018-09-11 19:43:03 +0800

    9-11

--- app/src/main/java/friendgoods/vidic/com/generalframework/mine/CustomDialogBottom.java
+++ app/src/main/java/friendgoods/vidic/com/generalframework/mine/CustomDialogBottom.java
@@ -2,25 +2,29 @@ package friendgoods.vidic.com.generalframework.mine;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import friendgoods.vidic.com.generalframework.R;
 
 public class CustomDialogBottom extends PopupWindow{
 
     private View dialogView;
     private EditText et_number;
-    private Button cancelBtn, confirmBtn;
     private ImageView goodsicon,add,reduce,close,next;
     private TextView name,price;
 
-    public CustomDialogBottom(Activity context, final BottomDialogOnclickListener bottomDialogOnclickListener) {
+    public CustomDialogBottom(final Activity context, final BottomDialogOnclickListener bottomDialogOnclickListener) {
         super(context);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         //
@@ -47,21 +51,76 @@ public class CustomDialogBottom extends PopupWindow{
         reduce.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                et_number.setText();
+                String aim=et_number.getText().toString().trim();
+                String reg="^[0-9]*$";
+                Pattern pattern = Pattern.compile(reg);
+                Matcher matcher = pattern.matcher(aim);
+                int i=-1;
+                if (matcher.matches()){
+                    i= Integer.parseInt(aim);
+                    i--;
+                }
+                if (i>=0){
+                    et_number.setText(i+"");
+                }else{
+                    Toast.makeText(context, "输入不合法", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                et_number.setText();
+                String aim=et_number.getText().toString().trim();
+                String reg="^[0-9]*$";
+                Pattern pattern = Pattern.compile(reg);
+                Matcher matcher = pattern.matcher(aim);
+                int i=-1;
+                if (matcher.matches()){
+                    i= Integer.parseInt(aim);
+                    i++;
+                }
+                if (i>=0){
+                    et_number.setText(i+"");
+                }else{
+                    Toast.makeText(context, "输入不合法", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bottomDialogOnclickListener.onPositiveClick("", CustomDialogBottom.this);
+                String number = et_number.getText().toString().trim();
+                String reg="^[0-9]*$";
+                Pattern pattern = Pattern.compile(reg);
+                Matcher matcher = pattern.matcher(number);
+                if (matcher.matches()){
+                    Toast.makeText(context, "NEXT", Toast.LENGTH_SHORT).show();
+//                    bottomDialogOnclickListener.onPositiveClick("", CustomDialogBottom.this);
+                }
             }
         });
+
+
+        this.setContentView(dialogView);
+        this.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        this.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
+        this.setFocusable(true);
+//        this.setAnimationStyle(R.style.DialogShowStyle); //设置SelectPicPopupWindow弹出窗体动画效果
+        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        this.setBackgroundDrawable(dw);
+
+//        dialogView.setOnTouchListener(new View.OnTouchListener() {
+//            public boolean onTouch(View v, MotionEvent event) {
+//                int height = dialogView.findViewById(R.id.pop_layout).getTop();
+//                int y = (int) event.getY();
+//                if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    if (y < height) { //mMenuView添加OnTouchListener监听判断获取触屏位置如果在选择框外面则销毁弹出框
+////                        dismiss();
+//                    }
+//                }
+//                return true;
+//            }
+//        });
     }
 }
