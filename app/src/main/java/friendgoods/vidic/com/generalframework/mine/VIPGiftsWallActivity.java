cd95a0f07239a24a6c2cb4b665f92dffb566d0c9
levy <2501908226@qq.com>
2018-09-14 17:15:14 +0800

    9-14 LVV

--- app/src/main/java/friendgoods/vidic/com/generalframework/mine/VIPGiftsWallActivity.java
+++ app/src/main/java/friendgoods/vidic/com/generalframework/mine/VIPGiftsWallActivity.java
@@ -11,5 +11,12 @@ public class VIPGiftsWallActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vipwall);
+
+        initView();
+    }
+
+    private void initView() {
+
+
     }
 }
